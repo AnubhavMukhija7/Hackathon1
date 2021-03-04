701724c613321331b9cd0a280c328b72a4c06594
Anubhav Mukhija <amakhija@ex2india.com>
2021-03-04 10:17:02 +0530

    Check

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -7,6 +7,26 @@ const Employee = {
   Dhiren: 'https://www.instagram.com/',
 };
 
+const Benefits = {
+  'All the benefits provided by the company that are currently active':
+    'benefit/',
+  'All the employees availing a particular benefit in the current year':
+    'benefit/:benefitName/employee',
+  'All the employees availing a particular benefit in the given year':
+    'benefit/:benefitName/:year/employee',
+  'Expense due to benefits for a particular emp in a particular year':
+    'benefit/employee/:id/expense,',
+  'Expense due to a particular benefit for a particular emp':
+    'benefit/employee/:id/expense/:benefitName',
+  'Benefits availed by a particular employee': 'benefit/employee/:id',
+  'Expenses due to benefits in current year': 'benefit/expense',
+  'Expenses due to a particular benefit in the current year':
+    'benefit/expense/:benefitName',
+  'Expense due to benefits in given year': 'benefit/expense/:year',
+  'Expense due to a particular benefit in the given year':
+    'benefit/expense/:benefitName/:year',
+};
+
 const Facilities = {
   a: 'https://www.amazon.com/',
   b: 'https://www.flipkart.com/',
@@ -22,15 +42,21 @@ const Expense = {
   bye: 'https://www.geeksforgeeks.org',
 };
 
-const title = ['Employee', 'Facilities', 'Vendors', 'Expense Summary'];
-const emp = [Employee, Facilities, Vendors, Expense];
+const title = [
+  'Employee',
+  'Benefits',
+  'Facilities',
+  'Vendors',
+  'Expense Summary',
+];
+const emp = [Employee, Benefits, Facilities, Vendors, Expense];
 
 const generateDOMList = (emp) => {
   for (const key in emp) {
     const noteEl = document.createElement('LI');
     const textEl = document.createElement('a');
     textEl.textContent = key;
-    textEl.setAttribute('href', emp[key]);
+    textEl.setAttribute('href', 'http://localhost:3000/' + emp[key]);
     noteEl.appendChild(textEl);
     options.appendChild(noteEl);
   }
