d7f2b3a31d22b64ab9ddff13793d9bd846cdd951
Aakash <arana@ex2india.com>
2021-03-04 11:43:24 +0530

    query fixed

--- API/5Repository/repo.emp.js
+++ API/5Repository/repo.emp.js
@@ -1,7 +1,8 @@
 import { makeConnection } from '../6Connection/connection.js';
 const request = await makeConnection();
 const findAllEmployee = async () => {
-    const query = 'Select * from Employee INNER JOIN EmployeeContact ON Employee.EmpId = EmployeeContact.EmpId INNER JOIN EmployeeAddress ON EmployeeContact.EmpId = EmployeeAddress.EmpId';
+    const query =
+        'Select * from Employee INNER JOIN EmployeeContact ON Employee.EmpId = EmployeeContact.EmpId INNER JOIN EmployeeAddress ON EmployeeContact.EmpId = EmployeeAddress.EmpId';
     const data = await request.query(query);
     return data.recordsets[0];
 };
@@ -18,7 +19,7 @@ const addEmployee = async (object) => {
     await request.query(insertIntoEmp);
     let data = await request.query(`Select * from Employee`);
     data = data.recordsets[0];
-    const newEmpId = (data).length;
+    const newEmpId = data.length;
     const insertIntoAddress = `Insert into EmployeeAddress (EmpId, StreetAddress1, StreetAddress2, City, District, PostalCode, State, Country, IsPermanent)  Values (${newEmpId}, '${object.StreetAddress1}', '${object.StreetAddress2}', '${object.City}', '${object.District}', ${object.PostalCode}, '${object.State}', '${object.Country}', ${object.IsPermanent})`;
     await request.query(insertIntoAddress);
     const insertIntoContact = `Insert into EmployeeContact (EmpId, Office,Mobile,LandLine) Values (${newEmpId}, ${object.Office},${object.Mobile},${object.LandLine})`;
@@ -27,13 +28,13 @@ const addEmployee = async (object) => {
     await request.query(insertIntoEmpBankDetails);
     const monthNo = (await request.query(`SELECT MONTH(GETDATE())`)).recordset[0][''];
     const year = (await request.query(`SELECT YEAR(GETDATE())`)).recordset[0][''];
-    const month = (await request.query(`select DateName( month , DateAdd( month , ${monthNo} , -1 ))`)).recordset[0][''];;
+    const month = (await request.query(`select DateName( month , DateAdd( month , ${monthNo} , -1 ))`)).recordset[0][''];
     const insertIntoEmployeePayhead = `Insert into EmployeePayhead (EmpId,Payhead,Month,Year) Values (${newEmpId},${object.Payhead},'${month}',${year})`;
     await request.query(insertIntoEmployeePayhead);
     return 'Record Inserted!\n';
 };
 
-const updateEmployee = async (object) => { 
+const updateEmployee = async (object) => {
     const data = await request.query(`Update Employees set ${object.Column} = '${object.Detail}' where EmployeeID=${object.EmployeeID}`);
     return 'Record Updated!\n' + data;
 };
@@ -53,7 +54,7 @@ const findAllEmployeeInTheGivenYear = async (year) => {
     Employee.EmpId = EmployeeContact.EmpId
     INNER JOIN EmployeeAddress ON
     EmployeeContact.EmpId = EmployeeAddress.EmpId 
-    where Employee.LeavingDate Is NULL OR (YEAR(Employee.LeavingDate) >= ${year} AND YEAR(Employee.JoiningDate)<=${year})`);
+    where ((YEAR(Employee.LeavingDate) >= ${year} OR YEAR(Employee.LeavingDate) Is NULL) AND YEAR(Employee.JoiningDate) <= ${year})`);
     return data.recordsets[0];
 };
 
